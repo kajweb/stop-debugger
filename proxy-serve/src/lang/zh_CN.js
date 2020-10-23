1e921bb32e90827cd904aac70b13f784925d1162
kajweb-740 <kajweb@163.com>
2020-10-24 05:20:31 +0800

    fix 启动提醒

--- proxy-serve/src/lang/zh_CN.js
+++ proxy-serve/src/lang/zh_CN.js
@@ -1,14 +1,9 @@
 module.exports = {
 	Diversion: "代理服务器",
-	HttpServer: "代理服务器",
-	HttpsServer: "代理服务器",
-	SNIServer: "代理服务器",
+	HttpServer: "http服务器",
+	HttpsServer: "https服务器",
+	SNIServer: "SNI服务器",
 	BeforeStart: "\n所有服务都已启动，但只有还差一步：",
-	TestStart: "\n请设置【系统代理】 \n并测试该服务是否可用。",
-
-	tracker: {
-		onGetZlibNoType: [
-
-		]
-	}
+	SettingStart: "\n请设置【系统代理】 ",
+	TestStart: "并测试该服务是否可用。"
 }
